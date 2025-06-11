69d16a581d59605de127d51771721de0de5b6028
SAVIO <savionaguicha@hotmail.com>
2025-06-11 05:16:11 -0500

    Tarea semana 2

---
+++ SEMANA 2/Técnicas de programación.py
@@ -0,0 +1,37 @@
+class bicicleta:
+    pass
+bicicleta = bicicleta()
+type(bicicleta)
+print(bicicleta)
+
+class avion:
+    pass
+Modelo = avion()
+Modelo.color = "blanco"
+Modelo.capacidad = "muchas"
+print("Mi avion es de color:", Modelo.color)
+
+class fabrica:
+    celulales = []
+    def __init__(self,celulares=[]):
+        sef. celulares = celulares
+        def fabrica(self,savio):
+            self.celulares.append(savio)
+            def visualizar(self):
+                for savio in self.celulares:
+                  print(savio)
+                  P
+
+
+
+
+
+
+
+
+
+
+
+
+
+
