68f28d36a8c9534c10ed54a4a2b1cbf445be5f42
SAVIO <savionaguicha@hotmail.com>
2025-07-13 22:54:53 -0500

    Tarea semana 7

---
+++ SEMANA 7/SEMANA 7 - Implementacion de Constructores y Destructores.py
@@ -0,0 +1,18 @@
+# crear clases raza perro
+
+
+class perro:
+    def __init__(self):
+     self. perro = open(perro_raza, "r")
+     def __del__(self):
+        self.perro.close()
+    print("perro cerrado correctamente")
+
+    def ladrar(self):
+        def __init__(self):
+            self.ladrar = open(ladrar_poco,"r")
+            def __del__(self):
+                self.ladrar.close()
+                print("ladrar cerrado correctamente")
+
+
