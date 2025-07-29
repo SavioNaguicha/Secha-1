8e30be582772fe4a777518b86925caff4b19483f
SAVIO <savionaguicha@hotmail.com>
2025-07-29 00:22:17 -0500

    Tarea semana 4

--- SEMANA 4/SEMANA 4 - Ejemplos del mundo real caracteristicas POO.py
+++ SEMANA 4/SEMANA 4 - Ejemplos del mundo real caracteristicas POO.py
@@ -1,17 +1,20 @@
 # Creación de clase  persona
 
+# Se define clase persona el constructor toma tres argumentos, nombre, apellido edad.
 class persona:
     def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
 
-
+# Se define el metodo saludar, y que devuelva un saludo que incluya nombre ya apellido de la persona.
     def saludar(self):
         return f"hola, mi nombre es {self.nombre} {self.apellido}"
 
-
+# Se crea una instancia de la clase persona llamado savio
 Savio = persona("Savio","Naguicha", 33,)
+
+# Imprimir codigo
 print(Savio.saludar())
 print(Savio.nombre)
 print(Savio.apellido)
